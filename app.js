43923553c034a22d5cd7e124cd267de739d6e095
Thomaz Feitoza <thomazfeitoza@gmail.com>
2019-11-25 08:35:53 -0300

    Improves checking rules and log messages

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const appPackage = require('./package');
 const config = require('./config');
 const publicIp = require('public-ip');
 const cloudflare = require('cloudflare')(config.cloudflare);
+const bunyan = require('bunyan');
+
+const log = bunyan.createLogger({
+  name: appPackage.name,
+  version: appPackage.version,
+  level: config.logLevel,
+  stream: process.stdout
+});
+
+let lastIpv4 = '';
 
 const getZone = async () => {
   let zones = await cloudflare.zones.browse();
@@ -25,31 +36,43 @@ const updateDns = async (zoneId, dnsRecordId, ipv4) => {
   };
 
   await cloudflare.dnsRecords.edit(zoneId, dnsRecordId, dnsData);
+  lastIpv4 = ipv4;
 
-  console.log(`IP address updated to ${ipv4}`);
+  log.info(`DNS A record with key ${config.cloudflare.domain} is now pointing to ${ipv4}`);
 };
 
 const checkAndUpdate = async () => {
-  try {
-    let ipv4 = await publicIp.v4();
+  let ipv4 = await publicIp.v4();
 
-    if (!ipv4) {
-      console.log('Public IP not found');
-      return;
-    }
-
-    let zone = await getZone();
-    let dnsRecord = await getADnsRecord(zone.id);
-
-    if (ipv4 === dnsRecord.content) {
-      return;
-    }
-
-    await updateDns(zone.id, dnsRecord.id, ipv4);
-
-  } catch (err) {
-    console.error(err);
+  if (!ipv4) {
+    log.warn('Public ipv4 not found');
+    return;
   }
+
+  if (ipv4 === lastIpv4) {
+    log.trace('Public ipv4 unchanged');
+    return;
+  }
+
+  let zone = await getZone();
+
+  if (!zone) {
+    log.warn('Cloudflare zone not found');
+  }
+
+  let dnsRecord = await getADnsRecord(zone.id);
+
+  if (!dnsRecord) {
+    log.warn('DNS A record not found');
+  }
+
+  if (ipv4 === dnsRecord.content) {
+    log.trace('DNS record does not need to be updated');
+    lastIpv4 = ipv4;
+    return;
+  }
+
+  await updateDns(zone.id, dnsRecord.id, ipv4);
 };
 
 const getInterval = async () => {
@@ -65,7 +88,7 @@ const startChecking = async () => {
     try {
       await checkAndUpdate();
     } catch (err) {
-      console.error(err);
+      log.error(err, 'Unhandled error');
     }
 
     await getInterval();
